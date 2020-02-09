87d52be734cd0f2b97431ad77ed71f318a90ac07
Duncan Temple Lang <duncan@r-project.org>
2020-02-08 18:14:52 -0800

    numPages() function for a QPDF or character object

---
+++ R/numPages.R
@@ -0,0 +1,19 @@
+
+
+numPages =
+function(file)    
+{
+    file = path.expand(file)
+    if(!file.exists(file))
+        stop(file, " does not exist")
+    
+    .Call("R_pdf_numPages", file)
+}
+
+
+numPages =
+function(doc)    
+{
+    doc = as(doc, "QPDF")
+    doc[[ getRoot(doc)$Pages ]]$Count
+}
