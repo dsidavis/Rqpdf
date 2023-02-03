72c04ca723bc3a9a611d9f4b4cf138126f40a942
Duncan Temple Lang <duncan@r-project.org>
2023-02-03 12:41:59 -0800

    enhance getPages to optionally resolve the references

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,21 @@
 getPages =
-function(doc)
+function(doc, resolve = TRUE)
 {
     doc = as(doc, "QPDF")
     ans = doc [[ getRoot(doc)$Pages ]]$Kids
-    structure(lapply(ans, function(x) { class(x) = c("PDFPage", "Page", class(x)); x}), class = "PDFPages")
+    ans = structure(lapply(ans, function(x) { class(x) = c("PDFPage", "Page", class(x)); x}), class = "PDFPages")
+
+    if(resolve)
+        resolvePages(ans, doc)
+    else
+        ans
+}
+
+resolvePages =
+function(pages, doc)    
+{
+    p3 = lapply(pages, function(ref) lapply(doc[[ref]]$Kids, function(r) doc[[r]]))
+    unlist(p3, recursive = FALSE)
 }
 
 
